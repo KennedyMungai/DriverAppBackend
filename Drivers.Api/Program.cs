717399e32f4125d2e7b94c8cd825903cf529d543
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-05 11:13:26 +0300

    Registered the configuration for JWT

--- Drivers.Api/Program.cs
+++ Drivers.Api/Program.cs
@@ -1,3 +1,5 @@
+using Drivers.Api.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
 
 var app = builder.Build();
 
