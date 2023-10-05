c65ed1dc5f2f442bf6b07e1419566c8d309e47b3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-05 12:45:34 +0300

    The basic registration logic

--- Drivers.Api/Controllers/AuthManagementController.cs
+++ Drivers.Api/Controllers/AuthManagementController.cs
@@ -1,4 +1,5 @@
 using Drivers.Api.Configurations;
+using Drivers.Api.Models.Dtos;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -19,5 +20,44 @@ public class AuthManagementController : ControllerBase
     {
         _userManager = userManager;
         _jwtConfig = _optionsMonitor.CurrentValue;
+    }
+
+    [HttpPost("Register")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Register([FromBody] UserRegistrationRequest requestDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return await Task.FromResult(BadRequest("Invalid Request Payload"));
+        }
+
+        var emailExist = await _userManager.FindByEmailAsync(requestDto.Email);
+
+        if (emailExist != null)
+        {
+            return await Task.FromResult(BadRequest("Email already exists"));
+        }
+
+        var newUser = new IdentityUser()
+        {
+            Email = requestDto.Email,
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword(requestDto.Password)
+        };
+
+        var isCreated = await _userManager.CreateAsync(newUser);
+
+        if (isCreated.Succeeded)
+        {
+            // TODO: Create a JWT Token
+
+            return await Task.FromResult(Ok(new RegistrationRequestResponse()
+            {
+                Result = true,
+                Token = ""
+            }));
+        }
+
+        return await Task.FromResult(BadRequest("Had an error creating the user"));
     }
 }
